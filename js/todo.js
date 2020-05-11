9e2a36b0373e695551f55a862e2997f7abe91449
Ignas Andriuškevičius <63236361+ignasandr@users.noreply.github.com>
2020-05-11 13:24:14 +0300

    Remove button

--- js/todo.js
+++ js/todo.js
@@ -11,9 +11,25 @@ function renderList(list) {
       <div class="status ${todoItem.status}"></div>
       <p class="description">${todoItem.description}</p>
       <div class="deadline">${todoItem.deadline}</div>
+      <div class="actions">
+        <div class="action remove">Remove</div>
+      </div>
     </div>`;
   }
   return listPlace.innerHTML += HTML
 }
 
 renderList(todo_list);
+
+const removeActions = document.querySelectorAll('.item .action.remove')
+
+for (let i=0; i<removeActions.length; i++) {
+  const removeElement = removeActions[i];
+  removeElement.addEventListener('click', actionRemoveTodoItem);
+}
+
+function actionRemoveTodoItem() {
+  // const parentItem = event.target.closest('.item');
+  const parentItem = event.path[2];
+  parentItem.remove();
+}
